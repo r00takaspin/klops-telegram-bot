13dbb5ba028b39501df02ae8b9e20c375baaed6a
Voldemar Duletskiy <voldemar.duletskiy@gmail.com>
2016-04-11 20:05:25 +0200

    refactor

--- bot.rb
+++ bot.rb
@@ -23,11 +23,14 @@ def send_popular(bot,message)
   end
 end
 
+def command_is?(cmd)
+  Menu::COMMAND_SYNONYMS.invert[message.text]==cmd || message.text.start_with?(cmd)
+end
+
 welcome_message = %{
 Оффициальный бот Klops приветствует тебя!
 Список комманд:
 \/start - начать общение с ботом
-\/help - список комманд
 \/news - список последних новостей
 \/popular - популярные новости за сутки
 \/subscribe - подписаться на важные новости (не более 3х за день)
@@ -65,29 +68,29 @@ Telegram::Bot::Client.run(TELEGRAM_BOT_TOKEN, logger: Logger.new($stdout)) do |b
     answers = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: menu.answers, one_time_keyboard: true)
 
     if Menu::COMMAND_SYNONYMS.invert.key?(message.text) || Menu::COMMAND_SYNONYMS.keys.include?(message.text) || message.text.start_with?('/start')
-      if Menu::COMMAND_SYNONYMS.invert[message.text]=='/start' || message.text.start_with?('/start')
+      if command_is? '/start'
         subscription_manager.subscribe(message.chat.id)
         menu.switch_subscription
         answers = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: menu.answers, one_time_keyboard: true)
         bot.api.send_message(chat_id: message.chat.id, text: welcome_message)
         bot.api.send_message(chat_id: message.chat.id, text: 'Я вас подписал рассылку новостей, не более трех в день, честное робо-слово',reply_markup: answers)
-      elsif Menu::COMMAND_SYNONYMS.invert[message.text]=='/news' || message.text == '/news'
+      elsif command_is? '/news'
         bot.api.send_message(chat_id: message.chat.id, text: 'Cписок последних новостей: ',reply_markup: answers)
         send_news(bot, message)
-      elsif Menu::COMMAND_SYNONYMS.invert[message.text]=='/popular' || message.text == '/popular'
+      elsif command_is? '/popular'
         bot.api.send_message(chat_id: message.chat.id, text: 'Популярные новости за сутки',reply_markup: answers)
         send_popular(bot, message)
-      elsif Menu::COMMAND_SYNONYMS.invert[message.text]=='/subscribe' || message.text == '/subscribe'
+      elsif command_is? '/subscribe'
         subscription_manager.subscribe(message.chat.id)
         menu.switch_subscription
         answers = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: menu.answers, one_time_keyboard: true)
         bot.api.send_message(chat_id: message.chat.id, text: 'Вы подписались на новости в чате, чтобы отписаться введите комманду /unsubscribe',reply_markup: answers)
-      elsif Menu::COMMAND_SYNONYMS.invert[message.text]=='/unsubscribe' || message.text == '/unsubscribe'
+      elsif command_is? '/unsubscribe'
         subscription_manager.unsubscribe(message.chat.id)
         menu.switch_subscription
         answers = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: menu.answers, one_time_keyboard: true)
         bot.api.send_message(chat_id: message.chat.id, text: 'Вы успешно отписались от рассылки новостей',reply_markup: answers)
-      elsif Menu::COMMAND_SYNONYMS.invert[message.text]=='/stop' || message.text == '/stop'
+      elsif command_is? '/stop'
         kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: true)
         subscription_manager.unsubscribe(message.chat.id)
         bot.api.send_message(chat_id: message.chat.id, text: "До свидания, #{message.from.first_name}", reply_markup: kb)
