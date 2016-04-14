632f937b369335c5e26e4ffeb7c4765cd2e11a9b
Voldemar Duletskiy <voldemar.duletskiy@gmail.com>
2016-04-14 15:04:48 +0200

    hotfixes

--- lib/commands/popular_command.rb
+++ lib/commands/popular_command.rb
@@ -12,10 +12,25 @@ class PopularCommand < BotCommand
     @answers = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: @menu.answers, one_time_keyboard: true, resize_keyboard: true)
     source = KlopsPopularNews.new(@period)
     source.fetch!
+    @bot.api.send_message(chat_id: @chat_id, text: desc_msg)
     if source.items.any?
       source.items.each do |item|
         @bot.api.send_message(chat_id: @chat_id, text: "#{item[0]} #{item[1]}",reply_markup: @answers)
       end
+    end
+  end
+
+  private
+  def desc_msg
+    case @period.to_sym
+      when :two_days
+        'Самые популярные новости за день'
+      when :four_days
+        'Самые популярные новости за три дня'
+      when :week
+        'Самые популярные новости за неделю'
+      else
+        'Самые популярные новости'
     end
   end
 end
