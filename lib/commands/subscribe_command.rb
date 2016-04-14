13ae139abf6d8eb31a75cbece95a3e3ff2e52bab
Voldemar Duletskiy <voldemar.duletskiy@gmail.com>
2016-04-14 18:26:50 +0200

    subscription message fix

--- lib/commands/subscribe_command.rb
+++ lib/commands/subscribe_command.rb
@@ -1,8 +1,11 @@
 class SubscribeCommand < BotCommand
+
+  SUBSCRIPTION_MESSAGE = 'Вы подписались на получение уведомлений о срочных новостях. Чтобы отписаться - нажмите соответствующую кнопку в меню.'
+
   def execute
     @subscription_manager.subscribe(@chat_id)
     switch_subscription
     @answers = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: @menu_answers, one_time_keyboard: true, resize_keyboard: true)
-    @bot.api.send_message(chat_id: @chat_id, text: 'Вы подписались на новости в чате, чтобы отписаться введите комманду /unsubscribe',reply_markup: @answers)
+    @bot.api.send_message(chat_id: @chat_id, text: SUBSCRIPTION_MESSAGE,reply_markup: @answers)
   end
 end
